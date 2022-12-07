a9ef02afcf81287156ed09e818eb83e7db6a1ae2
ALUNO <ALUNO@pcLegal>
2022-12-07 08:26:45 -0300

    Commit 2 e 3 - MAIN

--- GCMS Pratica GIT/src/HelloWorld.java
+++ GCMS Pratica GIT/src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("0");
-        System.out.println("1"); 
+        System.out.println("2 - MAIN");
+        System.out.println("3 - MAIN"); 
     }
 }
